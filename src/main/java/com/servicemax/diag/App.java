ba95c2b46506cb8609f2cfa1f79b912cb4962415
Nisvin <nisvin.shaffee@ge.com>
2018-05-28 16:46:24 +0530

    changed App.java 2

--- src/main/java/com/servicemax/diag/App.java
+++ src/main/java/com/servicemax/diag/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("Welcome back");
+        System.out.println("Welcome back Nisvin");
     }
 }
